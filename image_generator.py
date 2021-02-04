d7e3941f00cdfa98cc5beb90876d950913c79787
Md Shamim Seraj <shuvornb@gmail.com>
2021-02-04 13:12:08 -0500

    clustering applied to get most common color

--- image_generator.py
+++ image_generator.py
@@ -2,6 +2,20 @@ import time
 import cv2 as cv
 import numpy as np
 import DuckDuckGoImages as ddg
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from collections import Counter
+import os
+
+
+def show_img_compar(img_1, img_2):
+    f, ax = plt.subplots(1, 2, figsize=(10, 10))
+    ax[0].imshow(img_1)
+    ax[1].imshow(img_2)
+    ax[0].axis('off') #hide the axis
+    ax[1].axis('off')
+    f.tight_layout()
+    plt.show()
 
 
 def download_images():
@@ -15,31 +29,71 @@ def download_images():
     f.close()
 
 
+def generate_cluster_centers(img):
+    clt = KMeans(n_clusters=3)
+    clt.fit(img.reshape(-1, 3))
+    print(clt.labels_)
+    print(clt.cluster_centers_)
+
+
+def generate_palette(clusters):
+    width=300
+    palette = np.zeros((50, width, 3), np.uint8)
+    steps = width/clusters.cluster_centers_.shape[0]
+    for idx, centers in enumerate(clusters.cluster_centers_):
+        palette[:, int(idx*steps):(int((idx+1)*steps)), :] = centers
+    return palette
+
+
+def generate_palette_percentage(k_cluster):
+    width = 300
+    palette = np.zeros((50, width, 3), np.uint8)
+
+    n_pixels = len(k_cluster.labels_)
+    counter = Counter(k_cluster.labels_)  # count how many pixels per cluster
+    perc = {}
+    for i in counter:
+        print(counter[i])
+        perc[i] = np.round(counter[i] / n_pixels, 2)
+    perc = dict(sorted(perc.items()))
+
+    # for logging purposes
+    print(perc)
+    print(k_cluster.cluster_centers_)
+
+    step = 0
+
+    for idx, centers in enumerate(k_cluster.cluster_centers_):
+        palette[:, step:int(step + perc[idx] * width + 1), :] = centers
+        step += int(perc[idx] * width + 1)
+
+    return palette
+
+
 def generate_freq_map(img):
     dim = (500, 300)
     img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
     unique, counts = np.unique(img.reshape(-1, 3), axis=0, return_counts=True)
-    # max_index_col = np.argmax(counts, axis=0)
-    # print(unique[max_index_col])
-    # print(max(counts))
     return unique, counts
 
 
 if __name__ == '__main__':
-    #ddg.download('tomatoes', 'tomatoes', 5)
-    #img1 = cv.imread("tomatoes/725fd0a12bc94f0f88f6a689312183ba.jpg")
-    #img1 = cv.cvtColor(img1, cv.COLOR_BGR2RGB)
+    img_list = os.listdir('tomato')
+    clt = KMeans(n_clusters=3)
+    cluster_data = []
+    for x in img_list:
+        img = cv.imread("tomato/" + x)
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        clt.fit(img.reshape(-1, 3))
+        for z in clt.cluster_centers_:
+            cluster_data.append(z)
+        show_img_compar(img, generate_palette_percentage(clt))
 
-    #img2 = cv.imread("tomatoes/e18585644d2e4fe68e4be692cdfb1f2a.jpg")
-    #img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
+    print(len(cluster_data))
+    clt.fit(cluster_data)
+    print(clt.cluster_centers_)
+    img = cv.imread("sky/a24acfb8866e4724957e4cdfe026c99b.jpg")
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    show_img_compar(img, generate_palette_percentage(clt))
+    cv.imshow('Result', generate_palette_percentage(clt))
 
-    #unique_color_img1 = generate_freq_map(img1)
-    #unique_color_img2 = generate_freq_map(img2)
-
-    #col_img1 = []
-    #for x in unique_color_img1:
-    #    col_img1.append((x[0], x[1], x[2]))
-    #col_img2 = []
-    #for x in unique_color_img2:
-    #    col_img2.append((x[0], x[1], x[2]))
-    download_images()
