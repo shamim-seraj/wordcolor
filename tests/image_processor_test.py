bd35ae31a25ae93be3d2fb602adb8e65d9eac660
Md Shamim Seraj <shuvornb@gmail.com>
2021-02-10 15:06:48 -0500

    test code added for copy_items_in_list() and get_max_value_index() methods

--- tests/image_processor_test.py
+++ tests/image_processor_test.py
@@ -12,3 +12,15 @@ def test_calculate_percentage():
     dict = image_processor.calculate_percentage(clt)
     val_list = sorted(dict.values())
     assert val_list == [26, 30, 43]
+
+
+def test_copy_items_in_list():
+    assert image_processor.copy_items_in_list([3, 4, 2], [10, 20, 30], [10, 20, 30]) == [10, 20, 30, 10, 10, 10, 20, 20, 20, 20, 30, 30]
+    assert image_processor.copy_items_in_list([1, 2, 3], [5, 10, 15], []) == [5, 10, 10, 15, 15, 15]
+    assert image_processor.copy_items_in_list([1, 1, 1], [10, 20, 30], []) == [10, 20, 30]
+
+
+def test_get_max_value_index():
+    assert image_processor.get_max_value_index({0: 10, 1: 20, 2: 10}) == 1
+    assert image_processor.get_max_value_index({0: 20, 1: 20, 2: 10}) == 0
+    assert image_processor.get_max_value_index({0: 10, 1: 20, 2: 20}) == 1
